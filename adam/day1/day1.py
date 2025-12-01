ea80aeefa6f50256fdb5830a77dc715972351648
Adam <adamos9070@gmail.com>
2025-12-01 09:23:30 +0100

    task2

--- adam/day1/day1.py
+++ adam/day1/day1.py
@@ -16,8 +16,27 @@ def task_1(dane: list[str]) -> int:
             rotation = (rotation + int(ile)) % 100
     return counter
 
+def task_2(dane: list[str]) -> int:
+    rotation = 50
+    counter = 0
+    for i in dane:
+        kierunek = i[0]
+        ile = i[1:]
+        if kierunek == 'L':
+            for n in range(1, int(ile) + 1):
+                if (rotation - n) % 100 == 0:
+                    counter += 1
+            rotation = (rotation - int(ile)) % 100
+        else:
+            for n in range(1, int(ile) + 1):
+                if (rotation + n) % 100 == 0:
+                    counter += 1
+            rotation = (rotation + int(ile)) % 100
+    return counter
+
 def main() -> None:
     print(task_1(read_data('data.txt')))
+    print(task_2(read_data('data.txt')))
 
 if __name__ == '__main__':
     main()
