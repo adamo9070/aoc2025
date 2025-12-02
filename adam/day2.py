3f702bf40655871683b04a468f920e2791692b9d
Adam <adamos9070@gmail.com>
2025-12-02 21:02:21 +0100

    sigma

--- adam/day2.py
+++ adam/day2.py
@@ -2,10 +2,12 @@ def read_data(file) -> list[str]:
     with open(file, 'r') as f:
         return f.read().strip().split(',')
 
-def longest_substring(x: str) -> str:
-    pass
-
-# WARN: GOWNOGOWNOGOWNOGOWNOGOWNOGOWNO
+def dzielniki(x: str) -> list[int]:
+    dzielniki = []
+    for i in range(1, int(x)):
+        if len(x) % i == 0:
+            dzielniki.append(i)
+    return dzielniki
 
 def task_1(dane: list[str]) -> int:
     dane2 = []
@@ -19,20 +21,21 @@ def task_1(dane: list[str]) -> int:
                 result += n
     return result
 
+# Ściągnięte od karola pozdro 600 100 900
 def task_2(dane: list[str]) -> int: 
     dane2 = []
-    result = 0
+    results = []
     for i in dane:
         dane2.append(i.strip().split('-'))
     for i in dane2:
         for n in range(int(i[0]), int(i[1])+1):
-            if longest_substring(str(n)) == str(n)[0: len(longest_substring(str(n)))]:
-                for k in range(len(longest_substring(str(n))), len(str(n)), len(longest_substring(str(n)))):
-                    if str(n)[k:len(longest_substring(str(n)))] == longest_substring(str(n)):
-                        result += n
-    return result
+            for dz in range(1, len(str(n))):
+                if len(str(n)) % dz == 0:
+                    fragmenty = [str(n)[i:i + dz] for i in range(0, len(str(n)), dz)]
+                    if len(set(fragmenty)) == 1:
+                        results.append(n)
+    return sum(set(results))
 
-print(longest_substring('112312'))
 print(task_1(read_data('data/data1-example.txt')))
 print(task_1(read_data('data/data1.txt')))
 print('------task2--------')
