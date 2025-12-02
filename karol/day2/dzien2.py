95a8663ba0579897d264032099533b4e3047814a
karl0l <144963412+karl0l@users.noreply.github.com>
2025-12-02 21:47:43 +0100

    adios código innecesario: parte dos

--- karol/day2/dzien2.py
+++ karol/day2/dzien2.py
@@ -6,9 +6,7 @@ sum_of_invalid = 0
 for r in ranges:
     for ID in range(int(r[0]), int(r[1])+1):
         p = len(str(ID))
-        if str(ID)[0] == '0':
-            sum_of_invalid += ID
-        elif p % 2 == 0:
+        if p % 2 == 0:
             t = int(p/2)
             if str(ID)[t:] == str(ID)[:t]:
                 sum_of_invalid += ID
